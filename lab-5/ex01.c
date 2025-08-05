aba0630339021202a609973be1e20299c9f9d29e
miathaddar <68012016@kmitl.ac.th>
2025-08-05 04:31:11 +0000

    lab-5 exercises

---
+++ lab-5/ex01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+   int num, values[9]={0,0,0,0,0,0,0,0,0,0};
+   for(num=0;num<11;num++);
+   {
+    for(values=0;values<9;values++)
+   
+    printf("Enter the value",num, "here:");
+    scanf("%d",&values[9]);
+   }
+
+printf("Values in array are:",values[9],"\n");
+  
+return 0;
+}
+  
